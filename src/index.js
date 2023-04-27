6a6d25890719c8d7bbe90e595fccdceca58e7ad1
Vasantha-idexcel <vasantha.r@idexcel.com>
2023-04-27 14:08:54 +0530

    Prod Log Coverage

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const _ = require("lodash");
+let provided = false;
+let provided_count = 0;
+try {
+  provided = require("../scenarios.json");
+  _.keys(provided).forEach((i) => {
+    provided_count += provided[i].length;
+  });
+} catch {}
+const logged = require("../results.json");
+let data = "Si No,Scenario Code,Sequence,Logged\n";
+let serial = 1;
+
+_.forEach(logged, (value, key) => {
+  if (key != "total") {
+    list = _.sortBy(
+      _.map(_.concat(value.sequences, value.missed_sequences), (i) =>
+        _.toInteger(i)
+      )
+    );
+    list = _.map(list, (i) => _.toString(i));
+    _.forEach(list, (i) => {
+      if (_.includes(value.sequences, i)) {
+        data += `${serial},${key},${i},Yes\n`;
+        serial += 1;
+      } else {
+        data += `${serial},${key},${i},\n`;
+        serial += 1;
+      }
+    });
+  }
+});
+
+fs.writeFileSync("results.csv", data);
